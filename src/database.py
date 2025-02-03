807e9b18d7dc33d0b18ed53540c11d248befa119
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-03 10:00:09 +0100

    database.py
    v

--- src/database.py
+++ src/database.py
@@ -5,8 +5,6 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = 'postgresql://contactsdb_kq9t_user:tHoqR5wrUj5yvzLxnuPBfUHY2SnTrPYF@dpg-cuflgvdds78s73fm9ld0-a/contactsdb_kq9t
 '
 
-
-
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False,  bind=engine)
