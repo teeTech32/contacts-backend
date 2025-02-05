00db1272b804254d5943e9aec2da27fb846ea034
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-05 23:37:15 +0100

    database.py
    b

--- src/database.py
+++ src/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = 'postgresql://contactsdb_kq9t_user:tHoqR5wrUj5yvzLxnuPBfUHY2SnTrPYF@dpg-cuflgvdds78s73fm9ld0-a/contactsdb_kq9t'
+            
 
 engine = create_engine(DATABASE_URL)
 
